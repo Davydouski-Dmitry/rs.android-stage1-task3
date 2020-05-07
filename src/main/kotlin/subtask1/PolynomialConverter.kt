59f5218cb952fbf4152438807e1ed5b7d210af66
Dmitry <davidowski7777@gmail.com>
2020-05-07 20:47:49 +0300

    feat: add code subtask1

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,28 @@
 package subtask1
 
+import kotlin.math.absoluteValue
+
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
+
+        if(numbers.isEmpty()) return null;
+
+        var result =""
+
+        for ((index,number) in numbers.reversed().withIndex()){
+
+            if (number == 0) continue;
+
+            val absNumber = number.absoluteValue
+
+            val first = if (index == numbers.lastIndex) "" else if (number > 0) " + " else " - "
+            val second = if (index == 0) "$absNumber" else if (absNumber == 1) "x" else "${absNumber}x"
+            val third =  if (index < 2) "" else "^$index"
+
+            result = "$first$second$third$result"
+        }
+        return result
         throw NotImplementedError("Not implemented")
     }
 }
