cc7fb34ef0115437b667f8e45fda6b744406efbd
Dmitry <davidowski7777@gmail.com>
2020-05-07 21:12:42 +0300

    feat: add code subtask 3 and 5

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,31 @@
 package subtask3
 
+import kotlin.math.absoluteValue
+
 class ArrayCalculator {
 
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
+
+        val numbers = itemsFromArray.filterIsInstance<Int>().sortedBy { it.absoluteValue }.toMutableList()
+        if (numbers.isEmpty()) return 0
+
+        var sum = 1L
+        for (number in numbers) {
+            sum *= number
+        }
+
+        var numOfDiv = numbers.size - numberOfItems
+        while (numOfDiv > 0) {
+            val min = if (sum < 0) {
+                numbers.removeAt(numbers.indexOfFirst { it < 0 })
+            } else {
+                numbers.removeAt(numbers.indexOfFirst { numOfDiv >= 1 })
+            }
+            sum /= min
+            numOfDiv--
+        }
+        return sum.toInt()
+
         throw NotImplementedError("Not implemented")
     }
 }
--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -4,8 +4,46 @@ import kotlin.reflect.KClass
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
+
+        if (number.contains("-")) return null
+
+        val resultList = mutableListOf<String>()
+        var neighboursArray: Array<Char>
+
+        number.forEachIndexed { index, charNumber ->
+            val numberList = number.toCharArray().toMutableList()
+            getNeighbour(charNumber)?.let {
+                neighboursArray = it
+                neighboursArray.forEach { neighbourChar ->
+                    numberList[index] = neighbourChar
+                    var string: String = ""
+                    numberList.forEach {
+                        string += it
+                    }
+                    resultList.add(string)
+                }
+            }
+        }
+
+        return resultList.map { it }.toTypedArray()
+    }
+
+    fun getNeighbour(number: Char): Array<Char>? {
+        return when (number) {
+            '0' -> arrayOf('8')
+            '1' -> arrayOf('2', '4')
+            '2' -> arrayOf('1', '3', '5')
+            '3' -> arrayOf('2', '6')
+            '4' -> arrayOf('1', '5', '7')
+            '5' -> arrayOf('2', '4', '6', '8')
+            '6' -> arrayOf('3', '5', '9')
+            '7' -> arrayOf('4', '8')
+            '8' -> arrayOf('5', '7', '9', '0')
+            '9' -> arrayOf('6', '8')
+            else -> null
+        }
+
         throw NotImplementedError("Not implemented")
     }
 }
